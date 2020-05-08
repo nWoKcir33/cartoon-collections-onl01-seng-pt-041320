216577becd360b3e82e9aaf38c2fa350c2685f23
Rick Lim <rick.jason.a.lim@gmail.com>
2020-05-08 16:32:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,8 @@ def long_planeteer_calls(array)
   i += 1 
 end
 
+if 
+
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
