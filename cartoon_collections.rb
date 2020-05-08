4d5627b12b793adca20154f45042b6e85d33e63e
Rick Lim <rick.jason.a.lim@gmail.com>
2020-05-08 16:50:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,4 +27,5 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |type|
-    cheese_types.include? 
+    cheese_types.include? (type)
+  end
