b8b7eddc59285901960f174608a44a62a5c89350
Rick Lim <rick.jason.a.lim@gmail.com>
2020-05-08 16:30:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,8 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.each do |i|
-    if i.length
+  i = 0 
+  while i < array.length
 end
 
 def find_the_cheese# code an argument here
